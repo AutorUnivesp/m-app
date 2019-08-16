c324b2faa7c880754415ec90a209942e39d3dcca
AutorUnivesp <autor@univesp.br>
2019-08-16 17:44:46 -0300

    Updates

---
+++ precache-manifest.dc7e1872257892e948fafc6a65bf4d49.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ca716abdc279f377e626f2579891d2",
+    "url": "/m-app/index.html"
+  },
+  {
+    "revision": "a078d42a4192d0c69c68",
+    "url": "/m-app/static/css/main.8d39111d.chunk.css"
+  },
+  {
+    "revision": "d5b3a66587dbf1db0568",
+    "url": "/m-app/static/js/2.58447131.chunk.js"
+  },
+  {
+    "revision": "a078d42a4192d0c69c68",
+    "url": "/m-app/static/js/main.168899de.chunk.js"
+  },
+  {
+    "revision": "55a0693b587f7feaffda",
+    "url": "/m-app/static/js/runtime~main.209ac6fa.js"
+  },
+  {
+    "revision": "d046784375d95461aa6204318158f336",
+    "url": "/m-app/static/media/construction.d0467843.svg"
+  },
+  {
+    "revision": "c31cfa718b890263399721ad5296d71a",
+    "url": "/m-app/static/media/crying.c31cfa71.gif"
+  },
+  {
+    "revision": "b9e4960c1476c78043d499d975f86cdb",
+    "url": "/m-app/static/media/gif_home2.b9e4960c.gif"
+  },
+  {
+    "revision": "e8640bb3d7014b3f85121557693d1c93",
+    "url": "/m-app/static/media/logo_univesp_branco.e8640bb3.svg"
+  },
+  {
+    "revision": "56d4569053c4aab98b5c20e72d46157b",
+    "url": "/m-app/static/media/triangle.56d45690.gif"
+  },
+  {
+    "revision": "70e3f50480f15934f4d6f5b63f7aea4e",
+    "url": "/m-app/static/media/typing_transparent.70e3f504.gif"
+  }
+]);
