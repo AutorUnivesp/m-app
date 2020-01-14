1dfb02ab4b9211f66ad1eafe8b645829d970b4ae
AutorUnivesp <autor@univesp.br>
2020-01-14 10:45:42 -0300

    planilhas update

--- src/components/Planilhas/ControlePlanilha.jsx
+++ src/components/Planilhas/ControlePlanilha.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const ControlePlanilha = () => {
   return (
     <div className="mt-5">
-      <iframe src="https://docs.google.com/spreadsheets/d/19F5VU7k_7JUQ7dEzWBmE8Gxsfb6RmgrKzXSLFVJ4Heo/edit#gid=1941396699" width="100%" height="900" frameborder="0"></iframe>
+      <iframe src="https://docs.google.com/spreadsheets/d/1ztvBGdwMD0soA9t_R4vZWwUQZMIwl8HBChd-Njmi5jA/edit?ts=5e172807#gid=1941396699&fvid=300838408" width="100%" height="900" frameborder="0"></iframe>
     </div>
   )
 }
--- src/components/Planilhas/PlanilhaVideoAulas.jsx
+++ src/components/Planilhas/PlanilhaVideoAulas.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const PlanilhaVideoAulas = () => {
   return (
     <div className="mt-5">
-      <iframe src="https://docs.google.com/spreadsheets/d/1cvv0Pa3HmzqcqF-zO6aXiO6ifOAMRpF6uIXvT2PAPak/edit?ts=5d9cebd6#gid=0" width="100%" height="900" frameborder="0"></iframe>
+      <iframe src="https://docs.google.com/spreadsheets/d/175Kqfspi4_4mHAhci5WFXfkBDQGblZ2K1cIEGHtEym4/edit?ts=5e15e9e4#gid=1859623028" width="100%" height="900" frameborder="0"></iframe>
     </div>
   )
 }
